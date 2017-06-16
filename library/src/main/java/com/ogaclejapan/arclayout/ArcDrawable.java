8b4d245eef8e4c077b9cabb138532313134b6670
Ashish Goel <ashish.goel@squaredsd.com>
2017-06-16 16:20:08 +0530

    changes done

--- library/src/main/java/com/ogaclejapan/arclayout/ArcDrawable.java
+++ library/src/main/java/com/ogaclejapan/arclayout/ArcDrawable.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 ogaclejapan
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,96 +28,118 @@ import android.os.Build;
 
 public class ArcDrawable extends Drawable {
 
-  private final Paint arcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-  private Path arcPath = null;
-  private Arc arc;
-  private int arcRadius;
+    private final Paint arcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final Paint borderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Path arcPath = null, borderPath = null;
+    private Arc arc;
+    private int arcRadius, borderWidth;
 
-  public ArcDrawable(Arc arc, int radius, int color) {
-    this.arc = arc;
-    this.arcRadius = radius;
-    this.arcPaint.setColor(color);
-  }
-
-  public Arc getArc() {
-    return arc;
-  }
-
-  public void setArc(Arc arc) {
-    this.arc = arc;
-    ensurePath();
-  }
-
-  public int getRadius() {
-    return arcRadius;
-  }
-
-  public void setRadius(int radius) {
-    arcRadius = radius;
-    ensurePath();
-  }
-
-  public int getColor() {
-    return arcPaint.getColor();
-  }
-
-  public void setColor(int color) {
-    arcPaint.setColor(color);
-  }
-
-  @Override
-  public void setBounds(int left, int top, int right, int bottom) {
-    super.setBounds(left, top, right, bottom);
-    ensurePath(left, top, right, bottom);
-  }
-
-  @Override
-  public void draw(Canvas canvas) {
-    canvas.drawPath(arcPath, arcPaint);
-  }
-
-  @Override
-  public void setAlpha(int alpha) {
-    arcPaint.setAlpha(alpha);
-  }
-
-  @Override
-  public void setColorFilter(ColorFilter cf) {
-    arcPaint.setColorFilter(cf);
-  }
-
-  @Override
-  public int getOpacity() {
-    return PixelFormat.TRANSLUCENT;
-  }
-
-  @Override
-  public int getIntrinsicWidth() {
-    return arc.computeWidth(arcRadius);
-  }
-
-  @Override
-  public int getIntrinsicHeight() {
-    return arc.computeHeight(arcRadius);
-  }
-
-  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-  @Override
-  public void getOutline(Outline outline) {
-    if (arcPath == null || !arcPath.isConvex()) {
-      super.getOutline(outline);
-    } else {
-      outline.setConvexPath(arcPath);
+    public ArcDrawable(Arc arc, int radius, int color, int borderWidth, int borderColor) {
+        this.arc = arc;
+        this.arcRadius = radius;
+        this.arcPaint.setColor(color);
+        this.borderPaint.setColor(borderColor);
+        this.borderWidth = borderWidth;
     }
-  }
 
-  protected void ensurePath() {
-    final Rect r = getBounds();
-    ensurePath(r.left, r.top, r.right, r.bottom);
-  }
+    public Arc getArc() {
+        return arc;
+    }
 
-  protected void ensurePath(int left, int top, int right, int bottom) {
-    arcPath = arc.computePath(arcRadius, left, top, right, bottom);
-  }
+    public void setArc(Arc arc) {
+        this.arc = arc;
+        ensurePath();
+    }
+
+    public int getRadius() {
+        return arcRadius;
+    }
+
+    public void setBorderWidth(int width) {
+        borderWidth = width;
+        ensurePath();
+    }
+
+    public int getBorderWidth() {
+        return borderWidth;
+    }
+
+    public void setRadius(int radius) {
+        arcRadius = radius;
+        ensurePath();
+    }
+
+    public int getColor() {
+        return arcPaint.getColor();
+    }
+
+    public void setColor(int color) {
+        arcPaint.setColor(color);
+    }
+
+    public int getBorderColor() {
+        return borderPaint.getColor();
+    }
+
+    public void setBorderColor(int color) {
+        borderPaint.setColor(color);
+    }
+
+    @Override
+    public void setBounds(int left, int top, int right, int bottom) {
+        super.setBounds(left - borderWidth, top - borderWidth, right + borderWidth, bottom + borderWidth);
+        ensurePath(left, top, right, bottom);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawPath(borderPath, borderPaint);
+        canvas.drawPath(arcPath, arcPaint);
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        arcPaint.setAlpha(alpha);
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+        arcPaint.setColorFilter(cf);
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        return arc.computeWidth(arcRadius) + borderWidth;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        return arc.computeHeight(arcRadius) + borderWidth;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void getOutline(Outline outline) {
+        if (arcPath == null || !arcPath.isConvex()) {
+            super.getOutline(outline);
+        } else {
+            outline.setConvexPath(arcPath);
+        }
+    }
+
+    protected void ensurePath() {
+        final Rect r = getBounds();
+        ensurePath(r.left, r.top, r.right, r.bottom);
+    }
+
+    protected void ensurePath(int left, int top, int right, int bottom) {
+        arcPath = arc.computePath(arcRadius, left, top, right, bottom);
+        borderPath = arc.computeBorder(borderWidth + arcRadius, left, top, right, bottom);
+    }
 
 }
